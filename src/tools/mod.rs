b0dc7e5accbf85058e1a09d94679a3a89bd71b83
codelder <codelder@ibankapp.org>
2026-03-23 23:32:09 +0800

    feat(03-02): register TodoTool in tools module
    - Add mod todo declaration
    - Export TodoTool via pub use
    - Sort modules alphabetically

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,13 +1,15 @@
 mod bash;
+mod edit;
 mod glob;
 mod grep;
 mod read;
+mod todo;
 mod write;
-mod edit;
 
 pub use bash::BashTool;
+pub use edit::EditTool;
 pub use glob::GlobTool;
 pub use grep::GrepTool;
 pub use read::ReadTool;
+pub use todo::TodoTool;
 pub use write::WriteTool;
-pub use edit::EditTool;
